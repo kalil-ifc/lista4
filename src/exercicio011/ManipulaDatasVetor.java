1c067894576ec35a3a7ec0b7a9939abb93cca133
kalil-ifc <111343068+kalil-ifc@users.noreply.github.com>
2022-08-31 13:36:03 -0300

    Add files via upload

--- src/exercicio011/ManipulaDatasVetor.java
+++ src/exercicio011/ManipulaDatasVetor.java
@@ -1,4 +1,7 @@
 package exercicio011;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -8,6 +11,7 @@ public class ManipulaDatasVetor {
 		Scanner entrada=new Scanner(System.in);
 		Date data = new Date();
     	String guardaData = new String();
+    	boolean dataCerta = true;
     	
     	/** 
     	 * ENTRADA DA DATA NO FORMATO TEXTO
@@ -20,17 +24,27 @@ public class ManipulaDatasVetor {
     	/** 
     	 * VALIDAÇÃO DA DATA
     	 */
+    	try {
+	    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	        LocalDate d = LocalDate.parse(guardaData, formatter);
+	        dataCerta = true;
+    	} catch (DateTimeParseException e) {
+    		dataCerta = false;
+    	}
     	
-    	
-    	/**
-    	 * EXIBE A DATA COM CADA ITEM EM UMA POSICAO DO VETOR
-    	 */
-    	char[] vetorData = new char[11];
-    	vetorData = guardaData.toCharArray();
-    	
-    	for (int i=0; i<vetorData.length; i++) {
-			System.out.println(vetorData[i]);
-		}
+    	if (dataCerta) {
+    		/**
+        	 * EXIBE A DATA COM CADA ITEM EM UMA POSICAO DO VETOR
+        	 */
+        	char[] vetorData = new char[11];
+        	vetorData = guardaData.toCharArray();
+        	
+        	for (int i=0; i<vetorData.length; i++) {
+    			System.out.println(vetorData[i]);
+    		}
+    	} else {
+    		System.out.println("FIM");
+    	}
     	
 		entrada.close();
 	}
